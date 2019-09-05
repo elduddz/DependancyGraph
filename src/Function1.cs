1d18799b9cce17b750f749846455a642515ce5af
ElDuddz <elduddz@outlook.com>
2019-09-05 01:03:08 +0100

    moving things about

--- src/Function1.cs
+++ src/Function1.cs
@@ -51,6 +51,6 @@ namespace DependancyGraph
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
+        x`}
     }
 }
